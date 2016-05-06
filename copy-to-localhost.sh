8017b24898c97a7cca4df52b0481f0e642ae2ab7
Josh Newton <josh.newton@learnsci.co.uk>
2016-05-06 11:16:42 +0100

    Removed copy-to-localhost-script

--- copy-to-localhost.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ ! -d "/var/www/html/ebiolabs/question/type/lsciss" ]; then
- mkdir /var/www/html/ebiolabs/question/type/lsciss
-fi
-cp -rf ./* /var/www/html/ebiolabs/question/type/lsciss/
